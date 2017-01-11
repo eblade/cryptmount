8d96b3a1d092f776b880c2116e27f9d7cbe49746
Johan Egneblad <johan@egneblad.se>
2017-01-11 20:26:59 +0000

    Add csetup script and possibility to setup a disk easily

--- bin/mount_resource.sh
+++ bin/mount_resource.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -x
+
 resource_name=$1
 resource_path=$2
 mount_point=$3
+options=$4
+run_mkfs=${5:-no}
+
+echo "Checking $resource_name"
 if [[ ! -e "${resource_path}" ]]; then
     >&2 echo "Not mounted? Missing: ${resource_path}"
     exit 1
@@ -16,7 +21,6 @@ else
     fi
 fi
 
-echo "Mounting $resource_name"
 if [[ -b $resource_path ]]; then
     using_loop=0
     loopdev=$resource_path
@@ -25,6 +29,7 @@ else
     loopdev=$(sudo losetup -f)
     sudo losetup $loopdev $resource_path
 fi
+
 set +x
 sudo echo  # make sure sudo password is cached before pipe
 echo $(supergenpass.sh $resource_name 24) | sudo cryptsetup -c aes-xts-plain create --allow-discards $resource_name $loopdev
@@ -36,8 +41,14 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 set -x
-#sudo mkfs.btrfs /dev/mapper/$resource_name
-sudo mount $4 /dev/mapper/$resource_name $mount_point
+
+if [[ "$run_mkfs" == "yes" ]]; then
+    echo "Creating filesystem for $resource_name"
+    sudo mkfs.btrfs /dev/mapper/$resource_name
+fi
+
+echo "Mounting $resource_name"
+sudo mount $options /dev/mapper/$resource_name $mount_point
 if [[ $? -ne 0 ]]; then
     sudo cryptsetup remove $resource_name
     if [[ $using_loop == 1 ]]; then
